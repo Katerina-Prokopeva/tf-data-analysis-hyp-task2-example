cb1867d38b1e6da27dcc0ed59261bade6e5dd701
Екатерина Прокопьева <44777376+Katerina-Prokopeva@users.noreply.github.com>
2024-05-28 23:27:09 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,14 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 345280072 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+# Применим тест Манна-Уитни для проверки гипотезы
+    stat, p_value = mannwhitneyu(x, y, alternative='two-sided')
+    
+    # Уровень значимости
+    alpha = 0.06
+    
+    # Если p-значение меньше уровня значимости, отвергаем нулевую гипотезу
+    return p_value < alpha
