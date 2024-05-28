2b53435e7d613e86af387a98ed3cf4d798810892
Екатерина Прокопьева <44777376+Katerina-Prokopeva@users.noreply.github.com>
2024-05-28 23:41:15 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,7 +3,6 @@ import numpy as np
 
 
 chat_id = 345280072 # Ваш chat ID, не меняйте название переменной
-
 def solution(x: np.array, y: np.array) -> bool:
 # Применим тест Манна-Уитни для проверки гипотезы
     stat, p_value = mannwhitneyu(x, y, alternative='two-sided')
